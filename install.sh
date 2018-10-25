9667068295572faaf3b98fb2a24838438d8baa39
Даниил Пронин <mail@grawl.ru>
2018-10-26 02:25:00 +1000

    add dev server script, use python instead of python3

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@localhost', 'admin')" | python3 manage.py shell
+python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@localhost', 'admin')" | python manage.py shell
