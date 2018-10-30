a1d383192b5ed6ca0eb2d2ec04edcac4b68d4099
Andrey Ezhov <rybakisa@gmail.com>
2018-10-31 00:03:51 +1000

    fix typo in Calendar model

--- events_calendar/models.py
+++ events_calendar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Calendar(models.Model):
     name = models.CharField(max_length=255)
-    html_emded_code = models.TextField()
+    html_embed_code = models.TextField()
 
     def __str__(self):
         return str(self.name)
